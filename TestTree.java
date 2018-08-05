0fcc20e76c2d4b5b2abb6922db69ac3488ea29f4
Nanji <Nanji@users.noreply.github.com>
2018-08-05 08:58:32 +0800

    -a

---
+++ TestTree.java
@@ -0,0 +1,18 @@
+
+public class TestTree {
+	public static void main(String[] args) {
+
+		Tree tree = new Tree();
+		tree.addNode(8);
+		tree.addNode(3);
+		tree.addNode(10);
+		tree.addNode(1);
+		tree.addNode(6);
+		tree.addNode(14);
+		tree.addNode(4);
+		tree.addNode(7);
+		tree.addNode(13);
+		
+		tree.printNode();
+	}
+}
